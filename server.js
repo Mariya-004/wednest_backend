fa81a2c24aa0f5ab634e59114579f2e7a2d949ce
Mariya004 <mariyajyothy04@gmail.com>
2025-03-06 19:42:55 +0530

    fixing CORB error

--- server.js
+++ server.js
@@ -18,20 +18,13 @@ const URL = process.env.BACKEND_URL || 'http://localhost:3000';
 // Connect to database
 connectDB();
 
+app.use(express.json());
 
-app.use(cors({
-    origin: [
-        "http://localhost:3000", 
-        "https://wednest-frontend-orcin.vercel.app"
-    ],
-    credentials: true
-}));
 // ✅ CORS FIX
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "https://wednest-frontend-orcin.vercel.app"); 
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Content-Type", "application/json");
     res.header("Access-Control-Allow-Credentials", "true");
     
     if (req.method === "OPTIONS") {
@@ -40,8 +33,13 @@ app.use((req, res, next) => {
     next();
 });
 
-
-app.use(express.json());
+app.use(cors({
+    origin: [
+        "http://localhost:3000", 
+        "https://wednest-frontend-orcin.vercel.app"
+    ],
+    credentials: true
+}));
 // Serve uploaded files statically
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
