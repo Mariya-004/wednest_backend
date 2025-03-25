53562dabb860960dd76c7cf15050a3297325853a
Mariya004 <mariyajyothy04@gmail.com>
2025-03-25 21:46:19 +0530

    commit

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
 
 app.use(cors({
     origin: [
-        "http://localhost:3001", 
+        "http://localhost:3000", 
         "https://wednest-frontend-orcin.vercel.app"
     ],
     credentials: true
